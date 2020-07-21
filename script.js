d59da796fcaf454792d82541f944b478f14adde5
TaylorBranson <taylor.a.branson@gmail.com>
2020-07-20 20:23:31 -0500

    added form validation

--- script.js
+++ script.js
@@ -11,3 +11,33 @@
 </ol>
 <img src="${}">
 */
+function entryError(message) {
+   alert(message);
+   event.preventDefault();
+}
+
+function init() {
+   let form = document.getElementById("launchForm");
+   form.addEventListener("submit", function(event) {
+      let pilot = document.querySelector("input[name=pilotName");
+      let copilot = document.querySelector("input[name=copilotName");
+      let fuelLevel = document.querySelector("input[name=fuelLevel");
+      let cargoMass = document.querySelector("input[name=cargoMass");
+      // alert("Pilot Name: " + pilot.value);
+      if (pilot.value === '' || copilot.value === '' || fuelLevel.value === '' || cargoMass.value === '') {
+         entryError("All fields are required!");
+      } else if (isNaN(pilot.value) === false) {
+         entryError('Pilot name must be a string');
+      } else if (isNaN(copilot.value) === false) {
+         entryError('Co-pilot name must be a string');
+      } else if (isNaN(fuelLevel.value) === true) {
+         entryError('Fuel Level must be a number');
+      } else if (isNaN(cargoMass.value) === true) {
+         entryError('Cargo Mass must be a number');
+      }
+   });
+};
+
+
+
+window.onload = init;
